62a3b589f3fceba200a9b1f4bb5254beea4fbdb4
Allen Wu <allewun@gmail.com>
2018-03-28 15:34:12 -0700

    Initial Commit

---
+++ binary-search/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  binary-search
+//
+//  Created by Allen Wu on 2018-03-28.
+//  Copyright © 2018 allewun. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
