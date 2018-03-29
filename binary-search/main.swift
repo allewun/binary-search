ee9a9196ff6b78ed2026fa37df779925afbdf993
Allen Wu <allewun@gmail.com>
2018-03-28 18:42:30 -0700

    created SortedFileSearcher

--- binary-search/main.swift
+++ binary-search/main.swift
@@ -8,5 +8,83 @@
 
 import Foundation
 
-print("Hello, World!")
+public class SortedFileSearcher {
+    
+    private let handle: FileHandle
+    private let max: UInt64
+    private let bufferSize: Int = 128
+    private let delimiter: String = "\r\n"
+    
+    deinit {
+        handle.closeFile()
+    }
+    
+    init?(filePath: String) {
+        guard let handle = FileHandle(forReadingAtPath: filePath) else { return nil }
+        
+        handle.seekToEndOfFile()
+        
+        self.handle = handle
+        self.max = handle.offsetInFile
+        
+        handle.seek(toFileOffset: 0)
+    }
+    
+    public func search(for needle: String) -> String? {
+        print("Searching for \"\(needle)\"...")
+        return _binarySearch(needle: needle, lo: 0, hi: nil)
+    }
+    
+    private func _binarySearch(needle: String, lo: UInt64, hi: UInt64?) -> String? {
+        // seek to middle of file
+        let mid = ((hi ?? max) + lo) / 2
+        handle.seek(toFileOffset: mid)
+        
+        // fill text buffer
+        let data = handle.readData(ofLength: bufferSize)
+        guard let block = String(data: data, encoding: .utf8) else { return nil }
+    
+        // remove partial lines of text
+        let lines = block.components(separatedBy: delimiter)
+        guard lines.count >= 3 else { return nil }
+        let fullLines = lines.dropFirst().dropLast()
+        
+        // search within buffer
+        for line in fullLines {
+            if line.hasPrefix(needle) {
+                return line
+            }
+        }
+        
+        // no match found in buffer, so binary search!
+        let headAdjustment = UInt64(lines.first!.count)
+        let tailAdjustment = UInt64(lines.last!.count)
+        
+        guard (hi ?? max) - lo > bufferSize else { return nil }
+        
+        if needle > fullLines.first! {
+            // needle must exist in the latter half of the file
+            return _binarySearch(needle: needle, lo: mid + UInt64(bufferSize) - tailAdjustment, hi: hi)
+        }
+        else {
+            // needle must exist in the first half of the file
+            return _binarySearch(needle: needle, lo: lo, hi: mid + headAdjustment)
+        }
+    }
+}
 
+let arguments = CommandLine.arguments
+
+if arguments.count != 3 {
+    print("Usage: ./binary-search <string> <file>")
+    exit(1)
+}
+
+guard let searcher = SortedFileSearcher(filePath: arguments[2]) else { exit(1) }
+
+if let match = searcher.search(for: arguments[1]) {
+    print("Found match: \"\(match)\"")
+}
+else {
+    print("No match found for \"\(arguments[1])\"")
+}
