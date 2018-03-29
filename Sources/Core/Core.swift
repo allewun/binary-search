316792899948686ac19232a1651683d2aedb3915
Allen Wu <allewun@gmail.com>
2018-03-28 23:21:28 -0700

    move things around

--- Sources/Core/Core.swift
+++ Sources/Core/Core.swift
@@ -19,7 +19,7 @@ public class SortedFileSearcher {
         handle.closeFile()
     }
     
-    init?(filePath: String) {
+    public init?(filePath: String) {
         guard let handle = FileHandle(forReadingAtPath: filePath) else { return nil }
         
         handle.seekToEndOfFile()
@@ -75,18 +75,3 @@ public class SortedFileSearcher {
     }
 }
 
-let arguments = CommandLine.arguments
-
-if arguments.count != 3 {
-    print("Usage: ./binary-search <string> <file>")
-    exit(1)
-}
-
-guard let searcher = SortedFileSearcher(filePath: arguments[2]) else { exit(1) }
-
-if let match = searcher.search(for: arguments[1]) {
-    print("Found match: \"\(match)\"")
-}
-else {
-    print("No match found for \"\(arguments[1])\"")
-}
--- Sources/binary-search/main.swift
+++ Sources/binary-search/main.swift
@@ -1,1 +1,19 @@
-print("Hello, world!")
+import Foundation
+import Core
+
+let arguments = CommandLine.arguments
+
+if arguments.count != 3 {
+    print("Usage: ./binary-search <string> <file>")
+    exit(1)
+}
+
+guard let searcher = SortedFileSearcher(filePath: arguments[2]) else { exit(1) }
+
+if let match = searcher.search(for: arguments[1]) {
+    print("Found match: \"\(match)\"")
+}
+else {
+    print("No match found for \"\(arguments[1])\"")
+}
+
