a1ab121b8dfb5be1792c89433e709df030bdd24b
Allen Wu <allewun@gmail.com>
2018-03-31 12:56:42 -0700

    reorder args

--- Sources/binary-search/main.swift
+++ Sources/binary-search/main.swift
@@ -3,9 +3,9 @@ import Core
 import Commander
 
 command(
-    Argument<String>("file", description: "The sorted file to search."),
-    Argument<String>("string", description: "The string to search for.")
-) { file, string in
+    Argument<String>("string", description: "The string to search for."),
+    Argument<String>("file", description: "The *sorted* file to search.")
+) { string, file in
     guard let searcher = BinarySearch(in: file) else { exit(1) }
     guard let match = searcher.search(for: string) else { exit(1) }
     
